be08e3c62ac5ed804dc460677ab129078212b9a1
Conrad <conrisc@gmail.com>
2020-01-25 14:07:55 +0100

    Adds ArticleList component

---
+++ src/components/ArticleList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+
+import Styles from './ArticleList.module.scss';
+import { Article } from './Article';
+
+export function ArticleList(props) {
+    return (
+        <StaticQuery
+            query = {graphql `
+                query {
+                    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
+                        totalCount
+                        edges {
+                            node {
+                                id
+                                frontmatter {
+                                    title
+                                    image
+                                    date(formatString: "MMMM YYYY")
+                                }
+                                excerpt
+                            }
+                        }
+                    }
+                }`
+            }
+            render = {data => (
+                <div>
+                    {
+                        data.allMarkdownRemark.edges.map( ({node}) => <Article article={node} />)
+                    }
+                </div>
+            )}
+        />
+    );
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,17 @@
 import React from "react"
-import { graphql } from 'gatsby';
 
 import Styles from './index.module.scss';
 import { Layout } from '../components/Layout';
 import { Title } from '../components/Title';
-import { Article } from '../components/Article';
+import { ArticleList } from '../components/ArticleList';
 
-export default ({data}) => {
+export default () => {
     return (
         <Layout>
             <Title>Hi there, this is the Home page ;)</Title>
             <div className={Styles.content}>
-                {
-                    data.allMarkdownRemark.edges.map( ({node}) => <Article article={node} />)
-                }
+                <ArticleList />
             </div>
         </Layout>
     )
 }
-
-export const query = graphql `query {
-    allMarkdownRemark(sort: {fields: [frontmatter___date], order: DESC}) {
-        totalCount
-        edges {
-            node {
-                id
-                frontmatter {
-                    title
-                    image
-                    date(formatString: "MMMM YYYY")
-                }
-                excerpt
-            }
-        }
-    }
-}`;
-
